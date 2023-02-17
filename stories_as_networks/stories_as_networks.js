7691cc29c48a92240321284b1235e982caf02b4c
kima894 <kima894@newschool.edu>
2023-02-16 19:47:37 -0500

    linked to stories2 css

--- stories_as_networks/stories_as_networks.js
+++ stories_as_networks/stories_as_networks.js
@@ -1,1 +1,9 @@
-var boxed = document.getElementById
+// var door = document.getElementById('boxed')
+// console.log(door)
+
+function handledrag(){
+    console.log('slide')
+    var door = document.getElementById('boxed')
+    console.log(door) 
+
+}
