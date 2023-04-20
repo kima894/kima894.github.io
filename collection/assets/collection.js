e0771ebf929abae72915a5b808e3a86b2f30a368
kima894 <kima894@newschool.edu>
2023-04-20 18:52:11 -0400

    Dog licensed data with fetch

--- collection/assets/collection.js
+++ collection/assets/collection.js
@@ -10,30 +10,31 @@ const renderItems = (collection) => {
 
 
 		// You can make each element inside of that…
-		const itemTitle = document.createElement('h2') // Make an `h2`
-		itemTitle.innerHTML = item.title // Put the JSON title inside
-		listItem.appendChild(itemTitle) // And add it to the `li`!
+		const itemAnimalName = document.createElement('h2') // Make an `h2`
+		itemAnimalName.innerHTML = item.animalname // Put the JSON title inside
+		listItem.appendChild(itemAnimalName) // And add it to the `li`!
 
-		const itemImage = document.createElement('img') // And an image
-		itemImage.src = item.posterImage // Set the `src` attribute from the JSON
-		listItem.appendChild(itemImage) // And add that too
+		// const itemImage = document.createElement('img') // And an image
+		// itemImage.src = item.posterImage // Set the `src` attribute from the JSON
+		// listItem.appendChild(itemImage) // And add that too
 
 
 		// This can get annoying, so we can use “template literals” instead
 		const itemDetails =
 			`
-				<p>Released in <time>${item.year}</time></p>
-				<p><em>${item.runTime}</em></p>
-				<a href="${item.imdbLink}">
-					<p>${item.imdbRating} / 10 →</p>
+				<p>Born in <time>${item.animalbirth}</time></p>
+				<p>Breed name: <em>${item.breedname}</em></p>
+				<p>Gender: <em>${item.animalgender}</em></p>
+				<p>Licensed issue date: <em>${item.licenseissueddate}</em></p>
+				<p>Licensed Expired date: <em>${item.licenseexpireddate}</em></p>
 				</a>
 			`
 		listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
 
 		// You can build logic from your data, too
-		if (!item.alsoWriter) { // If this is `false`
-			listItem.classList.add('faded') // Add this class to the whole `li`
-		}
+		// if (!item.alsoWriter) { // If this is `false`
+		// 	listItem.classList.add('faded') // Add this class to the whole `li`
+		// }
 
 		collectionList.appendChild(listItem) // Then add the whole `li` into the `ul`
 	})
